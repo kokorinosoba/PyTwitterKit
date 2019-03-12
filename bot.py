cb937d2a0ed150751c46dfa5514673b42f9cecba
Kokorinosoba <34607448+Kokorinosoba@users.noreply.github.com>
2019-03-12 11:27:01 +0900

    oauthlibのインスタンス名をtwitterからrequestsに変更

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from setting import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
 class Twitter():
     def __init__(self):
-        self.twitter = OAuth1Session(API_KEY,
+        self.requests = OAuth1Session(API_KEY,
                                      API_SECRET_KEY,
                                      ACCESS_TOKEN,
                                      ACCESS_TOKEN_SECRET)
@@ -35,12 +35,12 @@ class Twitter():
     # Curate a collection of Tweets
 
     # Filter realtime tweets
-    
+
     # Get Tweet timelines
     def GET_statuses_home_timeline(self, count=10):
         url = "https://api.twitter.com/1.1/statuses/home_timeline.json?tweet_mode=extended"
         param = {"count": count}
-        result = self.twitter.get(url, params=param)
+        result = self.requests.get(url, params=param)
         print("statuses_home_timeline", result)
         self.print_tweet(result)
         return result
@@ -51,7 +51,7 @@ class Twitter():
     def GET_statuses_user_timeline(self, screen_name, count=10):
         url = "https://api.twitter.com/1.1/statuses/user_timeline.json?tweet_mode=extended"
         param = {"screen_name": screen_name, "count": count}
-        result = self.twitter.get(url, params=param)
+        result = self.requests.get(url, params=param)
         print("statuses_user_timeline", result)
         self.print_tweet(result)
         return result
@@ -60,20 +60,20 @@ class Twitter():
     def POST_statuses_destroy(self, id):
         url = "https://api.twitter.com/1.1/statuses/destroy/"+id+".json"
         params = {"id": id}
-        result = self.twitter.post(url, params=params)
+        result = self.requests.post(url, params=params)
         print("statuses_destroy", result)
 
     def POST_statuses_update(self, status):
         url = "https://api.twitter.com/1.1/statuses/update.json"
         param = {"status": status}
-        result = self.twitter.post(url, params=param)
+        result = self.requests.post(url, params=param)
         print("statuses_update", result)
 
     # Search Tweets
     def GET_search_tweets(self, search_word, count=10, result_type="recent"):
         url = "https://api.twitter.com/1.1/search/tweets.json?tweet_mode=extended"
         param = {"q": search_word, "count": count, "lang": "ja", "result_type": result_type}
-        result = self.twitter.get(url, params=param)
+        result = self.requests.get(url, params=param)
         print("search_tweets", result)
         self.print_tweet(result)
         return result
